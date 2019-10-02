c4bbec7640bb3550868a8cba6ec3e97623dae7d4
blair stacey <bobbias@debian>
2019-10-02 04:05:01 -0400

    import rect and add create room

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,4 +1,5 @@
 from map_objects.tile import Tile
+from map_objects.rect import Rect
 
 
 class GameMap:
@@ -18,6 +19,13 @@ class GameMap:
         tiles[32][22].block_sight = True
 
         return tiles
+
+    def create_room(self, room):
+        for x in range(room.x1 +1, room.x2):
+            for y in range(room.y1 + 1, room.y2):
+                self.tiles[x][y].blocked = False
+                self.tiles[x][y].block_sight = False
+
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
