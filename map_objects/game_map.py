e8564ebe708d7b139ccf04e90ddf957dfe2ba1b1
blair stacey <bobbias@debian>
2019-10-06 01:39:07 -0400

    complete room generation

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -25,6 +25,31 @@ class GameMap:
             x = randint(0, map_width - w - 1)
             y = randint(0, map_height - h - 1)
 
+            new_room = Rect(x, y, w, h)
+
+            for other_room in rooms:
+                if new_room.intersect(other_room):
+                    break
+            else:
+                self.create_room(new_room)
+
+                (new_x, new_y) = new_room.center()
+
+                if num_rooms == 0:
+                    player.x, player.y = new_x, new_y
+                else:
+                    (prev_x, prev_y) = rooms[num_rooms - 1].center()
+
+                    if randint(0, 1) == 1:
+                        self.create_h_tunnel(prev_x, new_x, prev_y)
+                        self.create_v_tunnel(prev_y, new_y, new_x)
+                    else:
+                        self.create_v_tunnel(prev_y, new_y, prev_x)
+                        self.create_h_tunnel(prev_x, new_x, new_y)
+
+                rooms.append(new_room)
+                num_rooms += 1
+
     def create_room(self, room):
         for x in range(room.x1 +1, room.x2):
             for y in range(room.y1 + 1, room.y2):
