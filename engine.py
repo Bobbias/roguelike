f81d1d444611e5bf8033907fd6cd59d83b6ea7eb
blair stacey <bobbias@debian>
2019-10-06 02:11:49 -0400

    implement FOV

--- engine.py
+++ engine.py
@@ -4,6 +4,7 @@ from input_handlers import handle_keys
 from entity import Entity
 from render_functions import render_all, clear_all
 from map_objects.game_map import GameMap
+from fov_functions import init_fov, recompute_fov
 
 LIMIT_FPS = 20  # only for realtime roguelikes
 
@@ -23,9 +24,15 @@ def main():
     ROOM_MIN_SIZE = 6
     MAX_ROOMS = 30
 
+    FOV_ALGORITHM = 0
+    FOV_LIGHT_WALLS = True
+    FOV_RADIUS = 10
+
     colors = {
         'dark_wall': tcod.Color(0, 0, 100),
-        'dark_ground': tcod.Color(50, 50, 150)
+        'dark_ground': tcod.Color(50, 50, 150),
+        'light_wall': tcod.Color(130, 110, 50),
+        'light_ground': tcod.Color(200, 180, 50)
     }
     
     player = Entity(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, '@', tcod.white)
@@ -44,13 +51,19 @@ def main():
     game_map = GameMap(MAP_WIDTH, MAP_HEIGHT)
     game_map.create_map(MAX_ROOMS, ROOM_MIN_SIZE, ROOM_MAX_SIZE, MAP_WIDTH, MAP_HEIGHT, player)
 
+    fov_recompute = True
+    fov_map = init_fov(game_map)
+
     key = tcod.Key()
     mouse = tcod.Mouse()
 
     while not tcod.console_is_window_closed():
         tcod.sys_check_for_event(tcod.EVENT_KEY_PRESS, key, mouse)
+        if fov_recompute:
+            recompute_fov(fov_map, player.x, player.y, FOV_RADIUS, FOV_LIGHT_WALLS, FOV_ALGORITHM)
 
-        render_all(con, entities, game_map, SCREEN_WIDTH, SCREEN_HEIGHT, colors)
+        render_all(con, entities, game_map, fov_map, fov_recompute, SCREEN_WIDTH, SCREEN_HEIGHT, colors)
+        fov_recompute = False
 
         tcod.console_flush()
 
@@ -66,6 +79,7 @@ def main():
             dx, dy = move
             if not game_map.is_blocked(player.x + dx, player.y + dy):
                 player.move(dx, dy)
+                fov_recompute = True
 
         if exit_game:
             return True
